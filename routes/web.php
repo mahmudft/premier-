1166b309491c9e6910ea16f2b6be6666db4d35e4
mahmudft <fatullayevm@gmail.com>
2024-10-05 18:10:55 +0400

    added routes and service providers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LeagueController;
+
+// Route to generate fixtures
+Route::get('/generate-fixtures', [LeagueController::class, 'generateFixtures']);
+
+// Route to simulate matches for a given week
+Route::get('/simulate-week/{week}', [LeagueController::class, 'simulateWeek']);
+
+// Route to view current league table
+Route::get('/league-table', [LeagueController::class, 'showLeagueTable']);
+
 Route::get('/', function () {
     return view('welcome');
 });
