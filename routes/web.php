471e72c52044c26197ee6e9262393f23cbbd298c
mahmudft <fatullayevm@gmail.com>
2024-10-06 21:46:13 +0400

    update routes and added new routes

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LeagueController;
 
-// Route to generate fixtures
-Route::get('/generate-fixtures', [LeagueController::class, 'generateFixtures']);
 
-// Route to simulate matches for a given week
-Route::get('/simulate-week/{week}', [LeagueController::class, 'simulateWeek']);
+Route::get('/simulate-week/{week}', [LeagueController::class, 'simulateMatchByWeek']);
 
-// Route to view current league table
+
 Route::get('/league-table/{week}', [LeagueController::class, 'showLeagueTable']);
+
+
+Route::get('/simulate-championship/{week}', [LeagueController::class, 'simulateChampionshipByWeek']);
 
 Route::get('/', function () {
     return view('welcome');
